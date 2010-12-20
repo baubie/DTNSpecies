533d7edb7ddbe510787620422f9b6dfcd25be92f
Brandon Aubie <aubiebn@hopper.(none)>
2010-12-20 18:33:20 -0500

    Great strides towards generalizing the run system

--- src/paper/run.py
+++ src/paper/run.py
@@ -1,49 +1,65 @@
-import simulations
+import simulations as sims
 import neuron
+import network
 import sys
 import netshow as ns
 
 ShowSpikes = True
 ShowVoltage = False
 
+# Define Paramters
 stims = [i for i in range(1,25,1)]
 param = [i*0.01 for i in range(1,11,1)]
-repeats = 15
+stims = [1, 5, 10]
+param = [0.01, 0.1]
+repeats = 1
+sim_time = 100
+netdef = network.DTN_AntiCoincidence
+modify = sims.AC_NMDA_BETA
 
+
+
+
+
+
+#####
+# EVERY BELOW HERE SHOULD NOT NEED TO BE MODIFIED
+
+
+# Setup and run simulations in parallel
 pc = neuron.h.ParallelContext()
 numProcs = int(pc.nhost())
-pc.runworker()
 ret = []
+pc.runworker()
 for i in range(numProcs):
-    pc.submit(simulations.NMDA_BETA,numProcs,i,stims,param,repeats,ShowSpikes,ShowVoltage)
+    pc.submit(sims.run,netdef,modify,numProcs,i,stims,param,repeats,sim_time,ShowSpikes,ShowVoltage)
 while pc.working():
     ret.append(pc.pyret())
 pc.done()
 
+# Combine all of the results together
 savedparams = []
 savedcells = []
-
 for i in range(numProcs):
     for r in ret:
         if r[0] == i:
             savedparams += r[1]
             savedcells += r[2]
-
 class Network:
     def __init__(self):
         savedparams = []
         savedcells = []
-
 net = Network()
 net.savedparams = savedparams
 net.savedcells = savedcells
 
 
+# Plot the mean number of spikes
 if ShowSpikes:
     ns.plot_mean_spikes(net, "IC-soma", "ac_nmda_beta.dat")
     ns.show()  # Comment out to just save the results to file
 
-# Plot the results
+# Plot the voltage traces
 if ShowVoltage:
     count = 0
     for a in param:
--- src/paper/simulations.py
+++ src/paper/simulations.py
@@ -4,31 +4,35 @@ import neuron
 import progress
 from math import ceil
 
-def sims_per_proc(procs, total):
-    return ceil(float(total)/procs)
 
-def NMDA_BETA_AC(AvailProcs,ThisProc,stims,param,repeats,ShowSpikes=False, ShowVoltage=False):
+def run(netdef,modify,procs,thisProc,stims,param,repeats,sim_time,SaveSpikes,SaveVoltage):
+    net = netdef()
     repeats = int(repeats)
-    net = network.DTN_AntiCoincidence()
     s = Simulation(net, randomseed=200)
     s.verbose = False
-    s.sim_time = 200
+    s.sim_time = sim_time
     s.dt = 0.025
-    count = 0
     total = len(stims)*len(param)*repeats
-    spp = sims_per_proc(AvailProcs, total)
-    start = ThisProc*spp
-    end = (ThisProc+1)*spp
+    spp = ceil(float(total)/procs)
+    start = thisProc*spp
+    end = (thisProc+1)*spp
+    count = 0
     for a in param: 
-        net.cells["IC"]["cells"][0].sec["dendE"].modifyNMDA(Beta=a, mg=0.5)
+        net = modify(net,a)
         for d in stims*repeats:
             progress.update(count-start, spp)
             if count >= start and count < end:
                 s.stim_dur = d 
                 s.run()
                 key = [a,d] 
-                net.savecells([["IC","soma"],["IC","dendE"]], key, spikes=ShowSpikes,conductance=False,current=False,voltage=ShowVoltage)
+                net.savecells([["IC","soma"],["IC","dendE"]], key, spikes=SaveSpikes,voltage=SaveVoltage)
             count += 1
+    progress.update(count-start, spp)
 
-    return [ThisProc,net.savedparams,net.savedcells]
+    r = [thisProc,net.savedparams,net.savedcells]
+    return r
 
+
+def AC_NMDA_BETA(net,a):
+    net.cells["IC"]["cells"][0].sec["dendE"].modifyNMDA(Beta=a, mg=0.5)
+    return net
