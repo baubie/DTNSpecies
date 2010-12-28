e29ef5591029333d55c0eff67087f1461c4ae710
Brandon Aubie <brandon@aubie.ca>
2010-12-27 19:46:20 -0500

    Fix indentation

--- src/paper/results/analyze.py
+++ src/paper/results/analyze.py
@@ -3,67 +3,69 @@ from subprocess import call
 
 if len(sys.argv) < 2:
     print "Error: Please provide a file to analze."
-quit()
+    quit()
 
-    import csv
+import csv
 
 
-    def unique(seq):
-        result = []
-        for a in seq:
+def unique(seq):
+    result = []
+    for a in seq:
         if a not in result:
-        result.append(a)
-        return result
+            result.append(a)
+    return result
 
 # Capture raw data
-        filename = sys.argv[1]
-        data = csv.reader(open(filename+".dat", 'rb'), delimiter=',')
-        x = []
-        y = []
-        y_names = []
-        first = True
-        for d in data:
-        if first:
+filename = sys.argv[1]
+data = csv.reader(open(filename+".dat", 'rb'), delimiter=',')
+x = []
+y = []
+y_names = []
+first = True
+for d in data:
+    if first:
         first = False
         y_names = d[1:]
         y = [[] for i in range(len(y_names))]
-        else:
-x.append(float(d[0]))
-    for i in range(len(y_names)):
-        y[i].append(float(d[i+1])) 
-
+    else:
+        x.append(float(d[0]))
         for i in range(len(y_names)):
-            y_names[i] = eval(y_names[i])
+            y[i].append(float(d[i+1])) 
 
-            listMode = False
-            numParams = 1
+for i in range(len(y_names)):
+    y_names[i] = eval(y_names[i])
 
-            if isinstance(y_names[0],list):
-                numParams = len(y_names[0])
-                listMode = True
+listMode = False
+numParams = 1
 
-                print "Found %d parameters" % numParams
-                if (len(sys.argv) < 2 + numParams):
-                    print "Error: Please specificy which parameters to use."
-                    print "Example: python analyze.py file 4 1 *"
-                    print "This will use the 3rd parameter as the y variable"
+if isinstance(y_names[0],list):
+    numParams = len(y_names[0])
+    listMode = True
 
-
-                    if listMode:
-                    y_params = [[] for i in range(len(y_names[0]))]
-                    for y in y_names:
-                    for i in range(numParams):
-                        y_params[i].append(y[i])
-
-                        for i in range(numParams):
-                            y_params[i] = unique(y_params[i])
+print "Found %d parameters" % numParams
+if (len(sys.argv) < 2 + numParams):
+    print "Error: Please specificy which parameters to use."
+    print "Example: python analyze.py file 4 1 *"
+    print "This will use the 3rd parameter as the y variable"
 
 
+if listMode:
+    y_params = [[] for i in range(len(y_names[0]))]
+    for y in y_names:
+        for i in range(numParams): 
+            y_params[i].append(y[i])
+    for i in range(numParams):
+        y_params[i] = unique(y_params[i])
 
-                            tuning_widths = []
-                            best_duration = []
-                            most_spikes = []
-                            for series in range(len(y)):
+    print y_params
+    quit()
+
+
+tuning_widths = []
+best_duration = []
+most_spikes = []
+
+for series in range(len(y)):
     best_duration.append(x[y[series].index(max(y[series]))])
     most_spikes.append(max(y[series]))
     max_pos = y[series].index(max(y[series]))
@@ -72,99 +74,96 @@ x.append(float(d[0]))
     width = 999
 
     if half in y[series]:
-    found = False
-    for i in range(max_pos, len(y[series])):
-        if found == True:
-        if y[series][i] != half: 
-        half_pos = i-1
-        break
-        if found == False:
-        if y[series][i] == half: found = True
+        found = False
+        for i in range(max_pos, len(y[series])):
+            if found == True:
+                if y[series][i] != half: 
+                    half_pos = i-1
+                    break
+            if found == False:
+                if y[series][i] == half: found = True
 
         width = x[half_pos] - x[max_pos] 
-        else:
+    else:
         for i in range(max_pos, len(y[series])):
             if y[series][i] < half:
-            m = (y[series][i]-y[series][i-1])/(x[i]-x[i-1])
-            distance = (half-y[series][i-1]) / m
-            width = x[i-1]+distance - x[max_pos]
-            break
+                m = (y[series][i]-y[series][i-1])/(x[i]-x[i-1])
+                distance = (half-y[series][i-1]) / m
+                width = x[i-1]+distance - x[max_pos]
+                break
+    tuning_widths.append(width)
 
-            tuning_widths.append(width)
 
-
-    f = open(filename+".analyze", 'w')
-    f.write('"Series","Width","Best Duration","Peak Spikes"\n')
-    for i in range(len(y_names)):
-        f.write("%s," % y_names[i])
-        f.write("%s," % tuning_widths[i])
-        f.write("%s," % best_duration[i])
-        f.write("%s" % most_spikes[i])
+f = open(filename+".analyze", 'w')
+f.write('"Series","Width","Best Duration","Peak Spikes"\n')
+for i in range(len(y_names)):
+    f.write("%s," % y_names[i])
+    f.write("%s," % tuning_widths[i])
+    f.write("%s," % best_duration[i])
+    f.write("%s" % most_spikes[i])
     f.write("\n")
-        f.close()
+    f.close()
 
 
-        def color(i, total):
-            points = [ [0.0, [1,0,0]],
-            [0.5, [0,0,1]],
-            [1.0, [0,0,0]],
-    ]
-     pos = float(i)/float(total)
-            col = []
-            last = points[0]
-            for p in points:
-            if p[0] == pos: 
+def color(i, total):
+    points = [ [0.0, [1,0,0]],
+    [0.5, [0,0,1]],
+    [1.0, [0,0,0]],
+             ]
+    pos = float(i)/float(total)
+    col = []
+    last = points[0]
+    for p in points:
+        if p[0] == pos: 
             col = p[1]
             break
-            if pos < p[0]: 
+        if pos < p[0]: 
             col = [last[1][i]+(p[1][i]-last[1][i])*(p[0]-pos)/(p[0]-last[0]) for i in range(3)]
             break
-            last = p
-            return 'rgb('+str(col[0])+','+str(col[1])+','+str(col[2])+')'
-
+        last = p
+    return 'rgb('+str(col[0])+','+str(col[1])+','+str(col[2])+')'
 
 
 
 # Output GLE file
-                    f = open(filename+".gle", "w")
-                    f.write('size 8 3\n')
-                    f.write('set font psh\n')
-                    f.write('set hei 0.15\n')
-                    f.write('set lwidth 0.02\n')
-                    f.write('begin graph\n')
-                    f.write('    size 4 3\n')
-                    f.write('    scale auto\n')
-                    f.write('    xtitle "Stimulus Duration (ms)"\n')
-                    f.write('    ytitle "Mean Spikes per Trial"\n')
-                    f.write('    x2axis off\n')
-                    f.write('    y2axis off\n')
-                    f.write('    data "'+filename+'.dat"\n')
-                    f.write('    xticks length -0.05\n')
-                    f.write('    yticks length -0.05\n')
-            for i in range(len(y)):
-                f.write('    d'+str(int(i+1))+' line color '+color(i,len(y))+'\n')
-                f.write('    key compact nobox\n')
-                f.write('end graph\n')
-                f.write('amove 4 0\n')
-                f.write('begin graph\n')
-                f.write('    size 4 3\n')
-                f.write('    scale auto\n')
-                f.write('    xtitle " (ms)"\n')
-                f.write('    ytitle "Width (ms) / Number of Spikes"\n')
-                f.write('    x2axis off\n')
-                f.write('    y2axis off\n')
-                f.write('    data "'+filename+'.analyze"\n')
-                f.write('    xticks length -0.05\n')
-                f.write('    yticks length -0.05\n')
-                f.write('    let d4 = x, d1/d3\n')
-                f.write('    d1 line marker fcircle msize 0.1 color rgb(1,0,0)\n')
-                f.write('    d2 line marker fsquare msize 0.1 color rgb(0,1,0)\n')
-                f.write('    d3 line marker fdiamond msize 0.1 color rgb(0,0,1)\n')
-                f.write('    d4 line marker ftriangle msize 0.1 color rgb(0,0,0) key "Norm Width"\n')
-                f.write('    key compact nobox pos br\n')
-    f.write('end graph\n')
-                f.close()
-
+f = open(filename+".gle", "w")
+f.write('size 8 3\n')
+f.write('set font psh\n')
+f.write('set hei 0.15\n')
+f.write('set lwidth 0.02\n')
+f.write('begin graph\n')
+f.write('    size 4 3\n')
+f.write('    scale auto\n')
+f.write('    xtitle "Stimulus Duration (ms)"\n')
+f.write('    ytitle "Mean Spikes per Trial"\n')
+f.write('    x2axis off\n')
+f.write('    y2axis off\n')
+f.write('    data "'+filename+'.dat"\n')
+f.write('    xticks length -0.05\n')
+f.write('    yticks length -0.05\n')
+for i in range(len(y)):
+    f.write('    d'+str(int(i+1))+' line color '+color(i,len(y))+'\n')
+f.write('    key compact nobox\n')
+f.write('end graph\n')
+f.write('amove 4 0\n')
+f.write('begin graph\n')
+f.write('    size 4 3\n')
+f.write('    scale auto\n')
+f.write('    xtitle " (ms)"\n')
+f.write('    ytitle "Width (ms) / Number of Spikes"\n')
+f.write('    x2axis off\n')
+f.write('    y2axis off\n')
+f.write('    data "'+filename+'.analyze"\n')
+f.write('    xticks length -0.05\n')
+f.write('    yticks length -0.05\n')
+f.write('    let d4 = x, d1/d3\n')
+f.write('    d1 line marker fcircle msize 0.1 color rgb(1,0,0)\n')
+f.write('    d2 line marker fsquare msize 0.1 color rgb(0,1,0)\n')
+f.write('    d3 line marker fdiamond msize 0.1 color rgb(0,0,1)\n')
+f.write('    d4 line marker ftriangle msize 0.1 color rgb(0,0,0) key "Norm Width"\n')
+f.write('    key compact nobox pos br\n')
+f.write('end graph\n')
+f.close()
 
 # View results in QGLE
 call(["qgle", filename+".gle"])
